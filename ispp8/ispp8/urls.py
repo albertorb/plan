981118a870455493182f5ebc72c316763d08524f
Albertitou26 <arincon1992@gmail.com>
2014-05-20 17:48:37 +0200

    [ISSUE WELCOME url] ahora solo se accede a welcome con / o nada

--- ispp8/ispp8/urls.py
+++ ispp8/ispp8/urls.py
@@ -30,5 +30,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin', include(admin.site.urls)),
-    url(r'', 'plan.views.welcome'),
+    url(r'^$', 'plan.views.welcome'),
 )
